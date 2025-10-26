feb85d7de4a8f4bebc5c2e15d3709f32e9d9129a
Sam Schmitt <sam@samschmitt.com>
2025-10-26 13:07:17 -0400

    Initial nixos config

---
+++ hardware-configuration.nix
@@ -0,0 +1,26 @@
+{ lib, ... }:
+{
+  # Tell nixpkgs what platform we are on (new-style)
+  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
+
+  # Virtio-friendly defaults for VMs
+  boot.initrd.availableKernelModules = [
+    "virtio_pci" "virtio_blk" "virtio_scsi" "xhci_pci" "sd_mod" "sr_mod"
+  ];
+
+  # Root filesystem
+  fileSystems."/" = {
+    device = 
+             
+             
+             ;
+    
+    
+    
+    fsType = "btrfs";
+  };
+
+  ;}
+
+  swapDevices = [ ];
+}
